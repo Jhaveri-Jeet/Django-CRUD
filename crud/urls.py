1506815e178606a04ce8d80cda0db7a42a48e033
Jhaveri-Jeet <jeetwork941@gmail.com>
2024-07-03 20:42:37 +0530

    application added

--- crud/urls.py
+++ crud/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
+    path("application/", include("crudapplication.urls"), name="index"),
 ]
---
+++ crudapplication/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrudapplicationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'crudapplication'
