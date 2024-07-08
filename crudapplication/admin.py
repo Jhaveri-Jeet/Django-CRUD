0152c76a87ab35d8aed6e689feb2b70858c76557
Jhaveri-Jeet <jeetwork941@gmail.com>
2024-07-08 19:55:22 +0530

    minor changes

--- crudapplication/admin.py
+++ crudapplication/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from .models import UserDetails
 
-# Register your models here.
+class AdminPaneDisplayUserDetails(admin.ModelAdmin):
+    list_display = ('name', 'email', 'password', 'image')
+    search_fields = ('name', 'email', 'password', 'image')
 
-
-class AdminUserDetails(admin.ModelAdmin):
-    list_display = ("name", "email", "password", "image")
-    search_fields = ("name", "email", "password")
-
-
-admin.site.register(UserDetails, AdminUserDetails)
+admin.site.register(UserDetails, AdminPaneDisplayUserDetails)
