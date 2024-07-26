83718f5be29e6a781216c415c712f2c48e303099
Jhaveri-Jeet <jeetwork941@gmail.com>
2024-07-26 16:47:18 +0530

    minor changes

--- crudapplication/models.py
+++ crudapplication/models.py
@@ -1,22 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
 class UserDetails(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to="images/")
 
     # def __str__(self):
     #     return self.name
+
 
 class UserMoreDetails(models.Model):
     user = models.OneToOneField(UserDetails, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=100)
     date_of_birth = models.DateField()
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
